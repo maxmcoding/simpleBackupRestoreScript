25662c3b613c9daad5b0799eb8c9b2322b1702dd
Max <6710551+maxmcoding@users.noreply.github.com>
2023-06-03 02:17:53 -0400

    Update restoreDbFromFiles.sh

--- restoreDbFromFiles.sh
+++ restoreDbFromFiles.sh
@@ -2,7 +2,7 @@
 
 #Direccion de ubicacion Backups
 DIRECTORIO=/opt/backup/
-
+DIRECTORIO_BACKUP=/xxx
 EXTENSION=.sql
 USUARIO=USUARIO_SECRETO_DB
 PGPASSWORD=CLAVE_SECRETA_DB
@@ -11,31 +11,31 @@ HOST=IP_O_URL_SERVIDOR_DB
 echo "Archivos de respaldo en la ruta $DIRECTORIO:"
 ARCHIVOS=$(ls $DIRECTORIO*$EXTENSION)
 echo "$ARCHIVOS"
-# exit
-
-# echo "Ingrese el número del archivo de respaldo que desea restaurar:"
-# read OPCION
-
-for i in "${DIRECTORIO}"*; do
-  ARCHIVO=$i
-  # echo "LOOP INI $ARCHIVO  "
-  
-  DB_FILENAME="${ARCHIVO:12}"
-  NOMBRE=$(echo $DB_FILENAME | sed 's/.sql//g')
  
+for i in "${DIRECTORIO}"*; do
+
+  clear=${i%.sql}
+  NOMBRE=${clear##*/}
+  ARCHIVO=${i}
+  echo "LOOP INI $ARCHIVO  "
+
+#  DB_FILENAME="${ARCHIVO:12}"
+#  NOMBRE=$(echo $DB_FILENAME | sed 's/.sql//g')
+
     # echo "Creando Base de datos $NOMBRE ..."
     echo "$HOST:5432:postgres:$USUARIO:$PGPASSWORD" >> ~/.pgpass
-	chmod 0600 ~/.pgpass
-	export PGPASSWORD=$PGPASSWORD  
-	  
-	echo "Creando Base de datos $NOMBRE ..."
+        chmod 0600 ~/.pgpass
+        export PGPASSWORD=$PGPASSWORD
+
+        echo "Creando Base de datos $NOMBRE ..."
     psql -U "$USUARIO"  -h "$HOST" -d "postgres" -c "CREATE DATABASE $NOMBRE "
 
     # Restore the database dump file
     echo "IMPORTANDO Base de datos $NOMBRE ..."
     psql -U $USUARIO -h $HOST -d $NOMBRE -f $ARCHIVO
-	  rm $ARCHIVO
-  # fi
+    mv $ARCHIVO $DIRECTORIO_BACKUP$NOMBRE
+# rm $ARCHIVO
+# fi
 
 done
 
